68395da91058110e3d1b441c4f19ab4b51acdbac
Ameo <me@ameo.link>
2015-11-02 07:15:32 +0000

    Worked on calculating moving averages

--- notes.md
+++ notes.md
@@ -1,9 +1,15 @@
+#ZADD sset score member
+- Adds member to sorted set sset with score score.
+
+#ZREM sset member
+- Removes member member from sorted set sset.
+
 #ZRANGE sset index1 index2
-- returns the members of a sorted set between index1 and index2
+- returns the members of a sorted set between index1 and index2.
 
 #ZRANGEBYSCORE sset score1 score2
 - Returns the members in a sorted set that have scores between the defined parameters. 
-- Adding in a ( before the score will make it exclusive.  So ZRANGEBYSCORE sset (5 10 will return all elements with score >5 and <= 10
+- Adding in a ( before the score will make it exclusive.  So ZRANGEBYSCORE sset (5 10 will return all elements with score >5 and <= 10.
 
 #ZRANK sset member
 - returns the index in a sorted set of the element.  
@@ -13,3 +19,6 @@
 
 #ZSCORE sset member
 - returns the score of a member of a sorted set.  
+
+#ZCOUNT sset
+- returns the number of members of a sorted set.
--- tick_database/helpers/db_utils.js
+++ tick_database/helpers/db_utils.js
@@ -5,6 +5,38 @@ client.on('error', function (err) {
   console.log('Error ' + err);
 });
 
+/*
+This function is called every time a new tick is recieved.  
+Its job is to call all other functions that perform calculations every time a new tick is recieved.  
+*/
 db.processTick = function(symbol, timestamp, ask, bid){
-	client.
+	calcSMAs(timestamp, symbol);
+}
+
+db.calcSMAs = function(timestamp, symbol){
+	calcSMA(timestamp, symbol, 5);
+}
+
+db.calcSMA = function(timestamp, symbol, range){
+	client.zrangebyscore("tick_bids_"+symbol.toLowerCase(),timestamp-range,timestamp,function(vals){
+		console.log(vals);
+		var total = 0;
+		var last;
+		for(var i=0;i<vals.length;i++){
+			if(i==0){
+				client.zscore("tick_bids_"+symbol.toLowerCase(),vals[i],function(score){
+					var diff = score-(timestamp-range);
+					total += diff*vals[i];
+					last = score;
+				});
+			}else{
+				client.zscore("tick_bids_"+symbol.toLowerCase(),vals[i],function(score){
+					var diff = score-last;
+					total += diff*vals[i];
+					last = score;
+				});
+			}
+		}
+		console.log(total/vals.length);
+	});
 }
